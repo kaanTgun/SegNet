bc0ea880c50752681e60495572e01e674816579b
kaanTgun <33357878+kaanTgun@users.noreply.github.com>
2021-01-24 10:00:09 -0500

    clean up

--- init.py
+++ init.py
@@ -17,9 +17,9 @@ def train():
 	Train Model
 	"""
 	# Hyperparameters
-	BATCH_SIZE 		 = 12
-	INPUT_IMAGE_SIZE = 180
-	EPOCS 			 = 10
+	BATCH_SIZE 		 = 24
+	INPUT_IMAGE_SIZE = 128
+	EPOCS 			 = 6
 
 	# Training data setup
 	T_img_Folder = "/content/Hands_ex/Training"
@@ -45,16 +45,14 @@ def train():
 	train_dataset = Structured_Dataset(txt_file=T_txt_File, root_dir=T_img_Folder, image_size=INPUT_IMAGE_SIZE)
 	val_dataset   = Structured_Dataset(txt_file=V_txt_File, root_dir=V_img_Folder, image_size=INPUT_IMAGE_SIZE)
 
-	train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-	val_data_loader   = torch.utils.data.DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True)
+	train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, num_workers=4, shuffle=True)
+	val_data_loader   = torch.utils.data.DataLoader(val_dataset, batch_size=BATCH_SIZE,num_workers=4, shuffle=True)
 
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # PyTorch v0.4.0
 	model = SegNet(3,22).to(device)
 	model_location = next(model.parameters()).device
 
 	optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-	mse = torch.nn.MSELoss(reduction = 'mean')
-
 	writer = SummaryWriter()
 
 	for epoch in range(EPOCS):
@@ -71,14 +69,19 @@ def train():
 			outputs = model(imageLs)
 			loss = 0
 			for o,l in zip(outputs, labels):
-				l.to(model_location)
-				loss += weighted_loss(model_location, outputs=o, labels=l)
+				loss += weighted_loss(model_location, outputs=o, labels=l.to(model_location))
 
 			writer.add_scalar(LOG_LOSS_T_PATH, loss, epoch)
 			loss.backward()
 			optimizer.step()
-			if i % 100 == 0:
+			
+			if (i % 100) == 0:
 				print(f"{epoch}-{i} Training Loss: {loss}")
+			
+			if (i % 1000) == 0:
+				OUTPUT_path = f'{OUTPUT_PATH}/Weights_E_{epoch}_i_{i}_loss_{loss}.pt'
+				torch.save(model, OUTPUT_path)
+				writer.flush()
 
 
 		# Validate
@@ -90,8 +93,7 @@ def train():
 
 			loss = 0
 			for o,l in zip(outputs, labels):
-				l.to(model_location)
-				loss += weighted_loss(model_location, outputs=o, labels=l)
+				loss += weighted_loss(model_location, outputs=o, labels=l.to(model_location))
 
 			writer.add_scalar(LOG_LOSS_V_PATH, loss, epoch)
 			if i % 100 == 0: 
