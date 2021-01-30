932f77af115ab5ed1fb488f790880b5b2fbcac44
kaanTgun <33357878+kaanTgun@users.noreply.github.com>
2021-01-30 11:45:34 -0500

    preprocessing update

--- init.py
+++ init.py
@@ -1,27 +1,22 @@
 from Model import SegNet
 import Utils
-from Utils import Structured_Dataset, weighted_loss
 
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
-from skimage import io
-import scipy
 import numpy as np
-
 import json
 import os
-
 
 def train():
 	"""
 	Train Model
 	"""
 	# Hyperparameters
-	BATCH_SIZE 		 = 2
-	INPUT_IMAGE_SIZE = 128
-	EPOCS 			 	= 6
-	NUM_WORKERS 	= 1
+	BATCH_SIZE 		 		= 22
+	INPUT_IMAGE_SIZE 	= 128
+	EPOCS 			 			= 6
+	NUM_WORKERS 			= 4
 
 	# Training data setup
 	T_img_Folder = "Hands_ex/Training"
@@ -38,17 +33,17 @@ def train():
 
 	Utils.create_log_folders(OUTPUT_PATH, LOG_LOSS_DIR, LOG_LOSS_T_PATH, LOG_LOSS_V_PATH)
 
-	train_dataset = Structured_Dataset(txt_file=T_txt_File, root_dir=T_img_Folder, image_size=INPUT_IMAGE_SIZE)
-	val_dataset   = Structured_Dataset(txt_file=V_txt_File, root_dir=V_img_Folder, image_size=INPUT_IMAGE_SIZE)
+	train_dataset = Utils.Structured_Dataset(txt_file=T_txt_File, root_dir=T_img_Folder, image_size=INPUT_IMAGE_SIZE)
+	val_dataset   = Utils.Structured_Dataset(txt_file=V_txt_File, root_dir=V_img_Folder, image_size=INPUT_IMAGE_SIZE)
 
 	train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=True)
 	val_data_loader   = torch.utils.data.DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-
-	device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # PyTorch v0.4.0
-	model = SegNet(3,22).to(device)
+	device 	= torch.device("cuda" if torch.cuda.is_available() else "cpu") # PyTorch v0.4.0
+	model 	= SegNet(3,22).to(device)
 	model_location = next(model.parameters()).device
 
+	loss_function = torch.nn.MSELoss()
 	optimizer = torch.optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
 	writer = SummaryWriter()
 
@@ -65,7 +60,7 @@ def train():
 			outputs = model(images)
 			loss = 0
 			for o,l in zip(outputs, labels):
-				loss += weighted_loss(model_location, outputs=o, labels=l.to(model_location))
+				loss += loss_function(o, l.to(model_location))
 
 			writer.add_scalar(LOG_LOSS_T_PATH, loss, epoch)
 			loss.backward()
@@ -92,7 +87,7 @@ def train():
 
 				loss = 0
 				for o,l in zip(outputs, labels):
-					loss += weighted_loss(model_location, outputs=o, labels=l.to(model_location))
+					loss += loss_function(o, l.to(model_location))
 
 				writer.add_scalar(LOG_LOSS_V_PATH, loss, epoch)
 				if i % 10 == 0: 
